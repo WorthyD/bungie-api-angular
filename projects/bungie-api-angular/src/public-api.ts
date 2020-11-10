ccd1373097ccc6be4853d4f08a07bc2635c30e8f
WorthyD <danieljworthy@gmail.com>
2020-11-10 06:32:31 -0600

    initial commits

--- projects/bungie-api-angular/src/public-api.ts
+++ projects/bungie-api-angular/src/public-api.ts
@@ -5,3 +5,18 @@
 // export * from './lib/bungie-api-angular.service';
 // export * from './lib/bungie-api-angular.component';
 // export * from './lib/bungie-api-angular.module';
+export * from './lib/api.module';
+export * from './lib/model/models';
+export * from './lib/configuration';
+
+export * from './lib/api/app.service';
+export * from './lib/api/communityContent.service';
+export * from './lib/api/content.service';
+export * from './lib/api/default.service';
+export * from './lib/api/destiny2.service';
+export * from './lib/api/fireteam.service';
+export * from './lib/api/forum.service';
+export * from './lib/api/groupV2.service';
+export * from './lib/api/preview.service';
+export * from './lib/api/trending.service';
+export * from './lib/api/user.service';
