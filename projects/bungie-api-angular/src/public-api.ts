8bfe5d43512810a101d117cd048386aeeea152c9
WorthyD <danieljworthy@gmail.com>
2022-12-18 08:07:20 -0600

    updates

--- projects/bungie-api-angular/src/public-api.ts
+++ projects/bungie-api-angular/src/public-api.ts
@@ -1,3 +1,19 @@
 /*
  * Public API Surface of bungie-api-angular
  */
+export * from './lib/api.module';
+export * from './lib/model/models';
+export * from './lib/configuration';
+
+export * from './lib/api/app.service';
+export * from './lib/api/communityContent.service';
+export * from './lib/api/content.service';
+export * from './lib/api/default.service';
+export * from './lib/api/destiny2.service';
+export * from './lib/api/fireteam.service';
+export * from './lib/api/forum.service';
+export * from './lib/api/groupV2.service';
+export * from './lib/api/preview.service';
+export * from './lib/api/tokens.service';
+export * from './lib/api/trending.service';
+export * from './lib/api/user.service';
